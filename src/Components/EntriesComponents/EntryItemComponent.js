068a557054403e05004c0a8295dfc278a8e5a536
211211 <nhquan307@outlook.com>
2019-10-25 02:47:16 +0700

    Fixed dummy images from unsplash when adding new entry

--- src/Components/EntriesComponents/EntryItemComponent.js
+++ src/Components/EntriesComponents/EntryItemComponent.js
@@ -7,6 +7,9 @@ import {newLocaleDate} from '../../Utils/utils'
 const ESCAPE_KEY = 27;
 const ENTER_KEY = 13;
 
+const image1 = 'https://unsplash.com/assets/api/photos-new-grid-0d4acd4dc0bebf39f8e8ad9986446ea11fa5609c047a923d8b81447b36bb9178.jpg'
+const image2 = 'https://unsplash.com/assets/api/search-minimal-f397c39212f9dd527c03be6550f2140c888f41e9f23ea8aa0591348101b157a5.jpg'
+const image3 = 'https://unsplash.com/assets/api/search-wanderlust-9b9f4877226c797c89e67a1307764855e67fd3a97bd8938090858efa745675ab.jpg'
 @observer
 class EntryItemComponent extends React.Component {
 	@observable editText = '';
@@ -14,19 +17,23 @@ class EntryItemComponent extends React.Component {
 	render() {
 		const { entry } = this.props;
 		console.log({entry})
+		const multipleBackgroundsGradientColour = `${entry.bgColour},
+			url(${image1}) left no-repeat,
+			url(${image2}) center no-repeat,
+			url(${image3}) right no-repeat`
 		return (
 			<li>
 				<div style={{
 					height: 120,
 					width: 339,
-					border: '1px solid red',
 					borderRadius: 10,
 					display: 'flex',
 					alignItems: 'center',
 					flexDirection: 'column',
 					margin: 25,
 					justifyContent: 'center',
-					background: entry.bgColour || 'none'
+					background: multipleBackgroundsGradientColour,
+					backgroundSize: 'contain',
 				}}>
 					<div style={{
 						fontSize: 27,
---
+++ src/Constants/Images.js
@@ -0,0 +1,5 @@
+export default [
+  'https://unsplash.com/assets/api/photos-new-grid-0d4acd4dc0bebf39f8e8ad9986446ea11fa5609c047a923d8b81447b36bb9178.jpg',
+  'https://unsplash.com/assets/api/search-minimal-f397c39212f9dd527c03be6550f2140c888f41e9f23ea8aa0591348101b157a5.jpg',
+  'https://unsplash.com/assets/api/search-wanderlust-9b9f4877226c797c89e67a1307764855e67fd3a97bd8938090858efa745675ab.jpg',
+]
