2d24c260faafeebfd69a8a35115430492a8fe19e
211211 <nhquan307@outlook.com>
2019-10-29 02:24:07 +0700

    Correct gradient degree

--- src/Constants/Colours.js
+++ src/Constants/Colours.js
@@ -1,6 +1,6 @@
-export const YELLOW = 'linear-gradient(to right, #EDFF0080, #FF660080)'
-export const GREEN = 'linear-gradient(to right, #0088FF80, #00FF7780)'
-export const PURPLE = 'linear-gradient(to right, #BD00FFBF, #FFD500BF)'
-export const BLUE = 'linear-gradient(to right, #0088FFBF, #BD00FFBF)'
+export const YELLOW = 'linear-gradient(110deg, #EDFF0080, #FF660080)'
+export const GREEN = 'linear-gradient(109deg, #0088FF80, #00FF7780)'
+export const PURPLE = 'linear-gradient(109deg, #BD00FFBF, #FFD500BF)'
+export const BLUE = 'linear-gradient(109deg, #0088FFBF, #BD00FFBF)'
 
 export default [YELLOW, GREEN, PURPLE, BLUE]
