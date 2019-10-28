c0bff54dd2e4aa4adebe96cd33b59488edabba70
211211 <nhquan307@outlook.com>
2019-10-29 03:29:35 +0700

    Be able to use async/await function with Babel 7

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 import { getSnapshot, destroy, onSnapshot } from 'mobx-state-tree'
 import { connectReduxDevtools } from 'mst-middlewares'
-import './index.css';
+import './index.css'
 
 import App from './Components/App'
 import TodoStore from './Models/TodosModel'
